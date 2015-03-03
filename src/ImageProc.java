8b7f08b1c5920ee3044c6d694c463ff578425707
johni <johnitravolta@gmail.com>
2015-03-04 00:49:08 +0200

    Emphasized the derivatives by taking into calculation for the pixel (x,y) these two: (x-1,y) (x+1, y), that does all the difference.
    Implemented gradientMagnitude method - works.

--- src/ImageProc.java
+++ src/ImageProc.java
@@ -7,83 +7,98 @@ import java.awt.image.BufferedImage;
 
 public class ImageProc {
 
-	public static BufferedImage scaleDown(BufferedImage img, int factor) {
-		if (factor <= 0)
-			throw new IllegalArgumentException();
-		int newHeight = img.getHeight()/factor;
-		int newWidth = img.getWidth()/factor;
-		BufferedImage out = new BufferedImage(newWidth, newHeight, img.getType());
-		for (int x = 0; x < newWidth; x++)
-			for (int y = 0; y < newHeight; y++)
-				out.setRGB(x, y, img.getRGB(x*factor, y*factor));
-		return out;
-	}
-	
-	public static BufferedImage grayScale(BufferedImage img) {
+    public static BufferedImage scaleDown(BufferedImage img, int factor) {
+        if (factor <= 0)
+            throw new IllegalArgumentException();
+        int newHeight = img.getHeight() / factor;
+        int newWidth = img.getWidth() / factor;
+        BufferedImage out = new BufferedImage(newWidth, newHeight, img.getType());
+        for (int x = 0; x < newWidth; x++)
+            for (int y = 0; y < newHeight; y++)
+                out.setRGB(x, y, img.getRGB(x * factor, y * factor));
+        return out;
+    }
+
+    public static BufferedImage grayScale(BufferedImage img) {
         int width = img.getWidth();
         int height = img.getHeight();
         BufferedImage out = new BufferedImage(width, height, img.getType());
         for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
                 out.setRGB(x, y, getGray(img.getRGB(x, y)));
-		return out;
-	}
+        return out;
+    }
 
     // returns the gray color of a given pixel's RGB value.
     private static int getGray(int rgb) {
         Color px = new Color(rgb);
-        int red = px.getRed();
-        int green = px.getGreen();
-        int blue = px.getBlue();
-        int avg = (red + green + blue) / 3;
+        int avg = (px.getRed() + px.getGreen() + px.getBlue()) / 3;
         return new Color(avg, avg, avg).getRGB();
     }
-	
-	public static BufferedImage horizontalDerivative(BufferedImage img) {
-		//TODO implement this
+
+    public static BufferedImage horizontalDerivative(BufferedImage img) {
+        //TODO implement this
         int width = img.getWidth();
         int height = img.getHeight();
         BufferedImage out = new BufferedImage(width, height, img.getType());
         BufferedImage gray = grayScale(img);
-        for (int x = 0; x < width - 1; x++)
-            for (int y = 0; y < height; y++) {
-                int g = gray.getRGB(x, y) & 0xFF;
-                int g1 = gray.getRGB(x + 1, y) & 0xFF;
-                int dx = (g - g1 + 255)/2;
+
+        for (int x = 1; x < width - 1; x++)
+            for (int y = 0; y < height; y++) { // calc dx as discretely as defined.
+                int dx = ((gray.getRGB(x - 1, y) & 0xFF) - (gray.getRGB(x + 1, y) & 0xFF) + 255) / 2;
                 out.setRGB(x, y, new Color(dx, dx, dx).getRGB());
             }
-		return out;
-	}
 
-	public static BufferedImage verticalDerivative(BufferedImage img) {
-		//TODO implement this
-        BufferedImage out = horizontalDerivative(rotateLeft(img));
-        return rotateRight(out);
-	}
+        // handle the top and bottom lines.
+        for (int y = 0; y < height; y++) {
+            out.setRGB(0, y, new Color(127, 127, 127).getRGB());
+            out.setRGB(width - 1, y, new Color(127, 127, 127).getRGB());
+        }
+        return out;
+    }
 
-	public static BufferedImage gradientMagnitude(BufferedImage img) {
-		//TODO implement this
-		return null;
-	}
-	
-	public static BufferedImage retargetSize(BufferedImage img, int width, int height) {		
-		//TODO implement this
-		return null;
-	}
-	
-	
-	public static BufferedImage showSeams(BufferedImage img, int width, int height) {
-		//TODO implement this
-		return null;
-		
-	}
+    public static BufferedImage verticalDerivative(BufferedImage img) {
+        //TODO implement this
+        return rotateRight(horizontalDerivative(rotateLeft(img)));
+    }
 
-    // rotates the picture 90 degrees left.
+    public static BufferedImage gradientMagnitude(BufferedImage img) {
+        //TODO implement this
+        int width = img.getWidth();
+        int height = img.getHeight();
+        BufferedImage out = new BufferedImage(width, height, img.getType());
+        BufferedImage gray = grayScale(img);
+
+        for (int x = 1; x < width - 1; x++)
+            for (int y = 1; y < height - 1; y++) {
+                int dx = (gray.getRGB(x - 1, y) & 0xFF) - (gray.getRGB(x + 1, y) & 0xFF);
+                int dy = (gray.getRGB(x, y - 1) & 0xFF) - (gray.getRGB(x, y + 1) & 0xFF);
+                int norma = (int) Math.sqrt(dx * dx + dy * dy);
+                if (norma > 255)
+                    out.setRGB(x, y, new Color(255, 255, 255).getRGB());
+                else
+                    out.setRGB(x, y, new Color(norma, norma, norma).getRGB());
+            }
+
+        return out;
+    }
+
+    public static BufferedImage retargetSize(BufferedImage img, int width, int height) {
+        //TODO implement this
+        return null;
+    }
+
+
+    public static BufferedImage showSeams(BufferedImage img, int width, int height) {
+        //TODO implement this
+        return null;
+
+    }
+
     private static BufferedImage rotateLeft(BufferedImage img) {
         return rotate(img, 1);
     }
 
-    // rotates the picture 90 degrees right.
     private static BufferedImage rotateRight(BufferedImage img) {
         return rotate(img, 0);
     }
@@ -92,7 +107,7 @@ public class ImageProc {
     private static BufferedImage rotate(BufferedImage img, int left) {
         if (left != 0 && left != 1) return img;
         int width = img.getWidth();
-        int height= img.getHeight();
+        int height = img.getHeight();
         BufferedImage out = new BufferedImage(height, width, img.getType());
         for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
@@ -100,8 +115,3 @@ public class ImageProc {
         return out;
     }
 }
-		
-	
-
-
-
